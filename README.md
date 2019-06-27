f7fbb59d4b64fd34da567784342d272fa21ba56e
Jose Luis Bossio <joseluisbossio@gmail.com>
2019-06-27 08:26:08 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lata
+# test homologacion
+# latan
---
+++ localizacion/partner-contact/setup/_metapackage/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo12-addons-oca-partner-contact",
+    description="Meta package for oca-partner-contact Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo12-addon-base_location',
+        'odoo12-addon-base_location_geonames_import',
+        'odoo12-addon-base_partner_sequence',
+        'odoo12-addon-base_vat_sanitized',
+        'odoo12-addon-partner_address_street3',
+        'odoo12-addon-partner_affiliate',
+        'odoo12-addon-partner_coc',
+        'odoo12-addon-partner_contact_personal_information_page',
+        'odoo12-addon-partner_disable_gravatar',
+        'odoo12-addon-partner_employee_quantity',
+        'odoo12-addon-partner_external_map',
+        'odoo12-addon-partner_fax',
+        'odoo12-addon-partner_firstname',
+        'odoo12-addon-partner_identification',
+        'odoo12-addon-partner_multi_relation',
+        'odoo12-addon-partner_vat_unique',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+    ]
+)
